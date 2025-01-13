aa5563b7cb03cd208b18357c7b922d727f09f339
CoolDoge <thedarkguard60@gmail.com>
2025-01-14 03:04:05 +0500

    Add CameraScreen.kt and CameraViewModel.kt

---
+++ app/src/main/java/com/dogiumlabs/pneumoniadetector/ui/camera/CameraScreen.kt
@@ -0,0 +1,11 @@
+package com.dogiumlabs.pneumoniadetector.ui.camera
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CameraScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
---
+++ app/src/main/java/com/dogiumlabs/pneumoniadetector/ui/camera/CameraViewModel.kt
@@ -0,0 +1,7 @@
+package com.dogiumlabs.pneumoniadetector.ui.camera
+
+import androidx.lifecycle.ViewModel
+
+class CameraViewModel(): ViewModel() {
+
+}
